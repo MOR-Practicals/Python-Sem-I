e7845a78809dabf2bb898d86209d2459e0257dc7
Manavv <77316251+MOR-Practicals@users.noreply.github.com>
2021-01-28 17:11:19 +0530

    Update Sinx.py

--- Sinx.py
+++ Sinx.py
@@ -4,12 +4,16 @@ def sin(x,n):
     sinx=0
     for i in range (n):    
         sinx+=((-1)**i)*(x**(2*i+1))/math.factorial(2*i+1)
-    print("Sin ({}) = {}".format(x,sinx))   
+    return sinx   
 
 def main():
-    n=int(input("ENTER THE VALUE OF n (FOR LARGE VALUES OVERFLOW ERROR MAY OCCUR):"))
-    x=float(input("ENTER THE VALUE OF x (RADIAN):"))      
-    sin(x,n)
+    n=int(input("ENTER THE VALUE OF n (NUMBER OF TERMS) (FOR LARGE VALUES OVERFLOW ERROR MAY OCCUR):"))
+    if(n<1):
+        print("n CANNOT BE LESS THAN 1")
+        main()
+    else:
+        x=float(input("ENTER THE VALUE OF x (RADIAN):"))      
+        print("Sin ({}) = {}".format(x,sin(x,n)))
 
 if __name__=="__main__": 
     main()
